c5201a56b2363b8430b7c6fa78858a448cbe23fd
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-10-18 18:47:57 +0900

    20231018_RWLbuilding, ThreadLocalStorage

---
+++ ServerCore/2_4_RWLbuilding.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ServerCore
+{
+    // 재귀적 락 허용할지 말지 결정해야함 여기선 YES => WriteLock->WriteLock ok / WriteLock->ReadLock ok / ReadLock->WriteLock no
+    // 스핀락 정책(5000번 -> yield())
+    class Lock
+    {                                       
+        const int EMPTY_FLAG = 0x00000000;  // 16진수는 각 0이 2^4의 값을 가짐, F는 4개 모두 1을 의미함
+        const int WRITE_MASK = 0x7FFF0000;  // 32 비트 중 제일 앞의 unused 빼고 15개
+        const int READ_MASK = 0x0000FFFF;   // 32 비트 중 뒤에서부터 16개
+        const int MAX_SPIN_COUNT = 5000;    // 스핀락 5000번 돌림
+                                            // 재귀적 락을 사용하기 위해 현재 관리되는 thread의 ID가 필요함으로 16진수로 추출 및 사용
+        int _flag = EMPTY_FLAG;  // 총 32비트중 [unused(1) = 음수표현 제일앞자리] [WirteThreadID(15)] [ReadCount(16)] 로 나눠서 사용
+                                 // 현재 플래그의 상태에 따라 EMPTH_FLAG면 락이 안걸려있는것, 다른 값이면 락이 걸려있는걸로 사용
+        int _writeCount = 0;    // WriteLock 잡은 스레드만 사용가능함으로 그냥 int변수로 사용
+
+        public void WriteLock()
+        {
+            // 동일 스레드(자기자신)가 WriteLock을 이미 획득하고 있는지 확인
+            int LockThreadID = (_flag & WRITE_MASK) >> 16;      // 현재 락을 획득한 스레드의 ID
+            if (Thread.CurrentThread.ManagedThreadId == LockThreadID) // 락을 가지고있는 스레드가 지금 여기 들어온 스레드랑 동일(재귀)하면
+            {
+                _writeCount++;
+                return;
+            }
+
+            // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권 획득
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;    // 현재 관리되는 스레드의 ID에 WriteMask를 씌울것이므로
+            while (true)                                                                // 16자리 밀어서 ReadMask 파트 비우고 씌움
+            {
+                for (int i = 0; i< MAX_SPIN_COUNT; i++)
+                {
+                    //시도 성공하면 return
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {                           // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때(EMPTY_FLAG 상태일 때), 경합해서 소유권 획득
+                        _writeCount = 1;
+                        return;
+                    }
+                }
+
+                Thread.Yield(); //5000번 돌려보고 안되면 일단 yield
+            }
+        }
+
+        public void WriteUnLock()
+        {
+            int LockCount = --_writeCount;  
+            if (LockCount == 0)             // 재귀적으로 여러번 걸린 락이 모두 해제된 경우 UnLock
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);    // _flag 깔끔하게 0으로 밀어줌
+        }
+
+        public void ReadLock() 
+        {
+            // 동일 스레드(자기자신)가 WriteLock을 이미 획득하고 있는지 확인
+            int LockThreadID = (_flag & WRITE_MASK) >> 16;      // 현재 락을 획득한 스레드의 ID
+            if (Thread.CurrentThread.ManagedThreadId == LockThreadID) // 락을 가지고있는 스레드가 지금 여기 들어온 스레드랑 동일(재귀)하면
+            {
+                Interlocked.Increment(ref _flag);   // ReadLock이므로 _flag의 값 +1
+                return;
+            }
+
+            // 아무도 WriteLock을 획득하고 있지 않으면 ReadCount를 1 늘린다
+            // == WRITE_MASK 파트가 0000이면 READ_MASK 파트를 1 늘림
+            while (true) 
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;             // READ_MASK 파트만 존재 ==> WriteLock이 안걸려 있다는 뜻 
+                }                           // 그러므로 WriteLock파트가 없으면 Read파트 1 늘림
+                // 동시에 들어온 스레드중 경합에 이긴 스레드는 자연스레 통과하고 사용, 진애 _flag값이 expected랑 변화하였으므로 한턴 대기 후 사용 
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnLock()
+        {
+            Interlocked.Decrement(ref _flag);   // _flag - 1 
+        }
+    }
+
+    internal class _2_4_RWLbuilding
+    {
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(delegate ()
+            {
+                for(int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnLock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnLock();
+                }
+            });
+
+            t1.Start();
+            t2.Start();
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ServerCore/2_5_ThreadLocalStorage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ServerCore
+{
+    internal class _2_5_ThreadLocalStorage
+    {
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>();
+        // static으로 선언되었으므로 힙영역에서 공유되어야 함
+        // 하지만 ThreadLocal로 선언해주었음 ==> ThreadName의 값을 변경하여도 '스레드 자신에게만 값이 해당값으로 변경됨'
+        // ==> 다른 스레드들이 접근하면 변한값이 반영안됨!!
+
+        static ThreadLocal<string> ThreadName2 = new ThreadLocal<string>(() => { return $"My name is {Thread.CurrentThread.ManagedThreadId}"; });
+        // valueFactory버전 ThreadLocal ==> value가 세팅이 안된 상태면 넣는값으로 세팅하고 아니면 value세팅 안함
+        // 위에 valueFactory 아닌 버전은 WhoAmI 실행할때마다 동일 스레드가 실행하더라도 새로 세팅하게 됨 이런 낭비를 방지
+
+        static void WhoAmI()
+        {
+            ThreadName.Value = $"My name is {Thread.CurrentThread.ManagedThreadId}";
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine(ThreadName.Value);
+        }
+
+        static void WhoAmI2()
+        {
+            bool repeat = ThreadName2.IsValueCreated;
+            if(repeat)
+                Console.WriteLine(ThreadName2.Value + $"{repeat}");
+            else
+                Console.WriteLine(ThreadName2.Value);
+        }
+
+        static void Main(string[] args)
+        {                    
+            // Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // Parallel.Invoke(method) : 각 메소드를 병렬적으로 각각의 task로 실행
+            // 각 WhoAmI에서 sleep동안 ThreadName을 변경해주고 있지만 TLS 변수 이므로 자기가 설정한 값으로만 나옴!
+
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI2, WhoAmI2, WhoAmI2, WhoAmI2, WhoAmI2, WhoAmI2, WhoAmI2);
+            // 스레드 풀 내에서 돌리므로 3개 돌리고 작업끝난걸로 돌리고 반복
+            // valueFactory버전을 통해 각 스레드가 반복해서 ThreadName2를 정해도 반복해서 설정하지않고 한번만 할 수 있게 됨!
+
+            ThreadName2.Dispose(); // 설정해둔 TLS 삭제
+        }
+
+    }
+}
